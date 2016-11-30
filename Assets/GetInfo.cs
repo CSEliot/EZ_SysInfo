d9d520efc9503db6953b684fe5b8c9616108a49b
Eliot Carney-Seim <eliot@kitelion.me>
2016-11-30 18:29:19 -0500

    Adding Unity Assets N Settings

---
+++ Assets/GetInfo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetInfo : MonoBehaviour {
+
+    public Text txt;
+    private SystemInfo sys;
+
+	// Use this for initialization
+	void Start () {
+        sys = new SystemInfo();
+        var info = typeof(SystemInfo).GetProperties();
+        txt.text += "\nOn: " + Application.platform;
+        for(int x = 0; x < info.Length; x++) {
+            if (info[x].CanRead) { 
+                txt.text += "\n" + info[x].Name + ": " + info[x].GetValue(sys, null);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
